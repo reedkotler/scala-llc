c98b1ee004defbc519ddf6e3ae0aebcd0cc79f57
reedkotler <reedkotler@hotmail.com>
2017-07-26 11:52:22 -0700

    finish sketching out basic idea for scanner. it can scan  ir for a null program now.

--- src/main/scala/scala_llc/Main.scala
+++ src/main/scala/scala_llc/Main.scala
@@ -2,10 +2,20 @@ package scala_llc
 
 import scala.io.Source
 
-class Token
 
 
 object Main {
+
+  class IrToken
+
+  case class Identifier(s: String) extends IrToken
+  case class StringLiteral(s: String) extends IrToken
+  case class Unparsed(s: String) extends IrToken
+  case object And extends IrToken
+  case object Assign extends IrToken
+  case object EndOfString extends IrToken
+  case object Comment extends IrToken
+  case object Target extends IrToken
 
   // Grammar based on https://github.com/llir/grammar
 
@@ -73,12 +83,12 @@ object Main {
     ('-' + result._1, result._2)
   }
 
-  def scanQuotedStringLiteral(s: String): (String, String) = {
+  def scanQuotedStringLiteral(s: String): (IrToken, String) = {
     val result = s.substring(1).span(x => x != '"')
-    (result._1, result._2.substring(1))
+    (StringLiteral(result._1), result._2.substring(1))
   }
 
-  val reservedWords = List(
+  val reservedWordsSet = Set(
     "add", "and", "any", "asm", "ashr", "c", "comdat", "constant", "declare",
     "define", "double", "exactmatch", "externally_initialized",
     "extractelement", "extractvalue", "fadd", "false", "fdiv", "float", "fmul",
@@ -89,33 +99,69 @@ object Main {
     "sub", "true" , "trunc", "type", "udiv", "undef", "urem", "void",
     "x86_fp80", "xor", "zeroinitializer", "zext")
 
+  val reservedWordsMap = Map[String, IrToken]("and" -> And, "target" -> Target)
+
   val singleCharOpSymbols = List("=", ",", "(", ")", "{", "}", "!", "<", ">", "[", "]")
   val multipleCharOpSymbols = List("...")
 
+  def scanIdOrReserved(s: String) : (IrToken, String) = {
+    val (s1, s2) = scanName(s)
+    if (reservedWordsMap.contains(s1))
+      (reservedWordsMap(s1), s2)
+    else
+      (Identifier(s1), s2)
+  }
 
-  def scanLine(s: String): List[Token] = {
-    var s_ = s.dropWhile(isWhiteSpace)
-    println(s_)
-    var result = Nil
-    result
+  def nextToken(s: String): (IrToken, String) = {
+    val s_ = s.dropWhile(isWhiteSpace)
+    if (s_.length() == 0)
+      return (EndOfString, "")
+    s_(0) match {
+      case x if isLetter(x)=> scanIdOrReserved(s_)
+      case '=' => (Assign, s_.substring(1))
+      case ';' => (EndOfString, "")
+      case '"' => scanQuotedStringLiteral(s_)
+      case '!' => (EndOfString, "")  // for now treat metadata as a comment
+      case _ =>     (Unparsed(s_), "")
+
+    }
+  }
+
+
+  def scanLine(s: String): List[IrToken] = {
+    val s_ = s.dropWhile(isWhiteSpace)
+    if (s_.length() == 0)
+      List()
+    else {
+      val (t, s__) = nextToken(s)
+      t match {
+        case EndOfString => List()
+        case _ => t :: scanLine(s__)
+      }
+    }
   }
 
 
   def unitTest(): Unit = {
     var s = scanQuotedStringLiteral("\"hello\"")
     println(s)
-    s = scanQuotedStringLiteral("\"hello\" some more")
-    println(s)
-    s = scanName("today")
-    println(s)
-    s = scanName("today=tomorrow")
-    println(s)
-    s = scanNegDecimalLit("-5")
-    println(s)
-    s = scanNegDecimalLit("-5+10")
-    println(s)
-    s = scanMetadataName("!mymeta")
-    println(s)
+    var t = scanQuotedStringLiteral("\"hello\" some more")
+    println(t)
+    println(scanName("today"))
+    println(scanName("today=tomorrow"))
+    println(scanNegDecimalLit("-5"))
+    println(scanNegDecimalLit("-5+10"))
+    println(scanMetadataName("!mymeta"))
+    var x = scanLine("a=b")
+    println(x)
+    x = scanLine("a = b; comment")
+    println(x)
+    x = scanLine("and")
+    println(x)
+    x = scanLine("; ModuleID = 'null.c'")
+    println(x)
+    x = scanLine("source_filename = \"null.c\"")
+    println(x)
   }
 
 
@@ -126,8 +172,7 @@ object Main {
     if (args.length == 1) {
       val filename = args(0)
       for (line <- Source.fromFile(filename).getLines) {
-        println(line)
-        scanLine(line)
+        println(scanLine(line))
       }
     }
     else {
